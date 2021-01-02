684df85cb0391bfc6a957f165e46dee1f951012c
Matthew_Dreemurr <hadadmat@gmail.com>
2021-01-02 16:11:10 +0100

    add error messahe

--- get_next_line_main.c
+++ get_next_line_main.c
@@ -6,7 +6,7 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/02 15:41:33 by mhadad            #+#    #+#             */
-/*   Updated: 2021/01/02 16:01:14 by mhadad           ###   ########.fr       */
+/*   Updated: 2021/01/02 16:10:49 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,13 @@ int	check_error(int fd, char **line )
 
 	if (-1 == (get_next_line(fd, line)))
 	{
-		printf("/!\\ [CRASH] /!\\");
+		printf("/!\\ [{Error}_check_error]: Return fonction /!\\\n");
 	}
-
+	else
+	{
+		printf("[{OK}_check_error]\n");
+	}
+	
 	return (1);
 }
 
@@ -30,6 +34,12 @@ int	main()
 	char	**line;
 
 	fd = 1;
+	if (!(line = malloc(sizeof(char) * 3)))
+	{
+		printf("[{Error}_main]: Malloc");
+		return (1);
+	}
 	check_error(fd, line);
+	free(line);
 	return (0);
 }
