c1b2668e77a9eebd279e450cf3441a6446953d2f
Matthew_Dreemurr <hadadmat@gmail.com>
2021-01-22 17:21:17 +0100

    wip

--- src/get_next_line.c
+++ src/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/02 14:15:17 by mhadad            #+#    #+#             */
-/*   Updated: 2021/01/22 16:40:54 by mhadad           ###   ########.fr       */
+/*   Updated: 2021/01/22 17:20:33 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,9 @@
 
 int	free_mem(char **buff)
 {
+#ifdef DEBUG
+printf(B_RED "\nFREE_MEM CALL FOR ERROR" RESET);
+#endif
 	free(*buff);
 	return (ERROR);
 }
@@ -49,34 +52,29 @@ int	get_next_line(int fd, char **line)
 
 	if (!line)
 		return (ERROR);
-#ifdef DEBUG
+																		#ifdef DEBUG
 printf(B_YEL "\nThe last buff" B_WHT " = |%s|\n" RESET, buff);
-#endif
+																		#endif
 	if (!(buff = malloc(sizeof(char) * (BUFFER_SIZE + 1))))
 		return (ERROR);
-	*buff = '\0';
-#ifdef DEBUG
-printf(B_YEL "\nAlloc & set to \'\\0\' " B_GRN "[Ok]\n" RESET);
-#endif
+	buff[0] = '\0';
 	while (!(gnl.len = eol_len(tmp, 0)))
 	{
-#ifdef DEBUG
-printf(B_YEL "gnl.len = " RESET "[%lu]\n", gnl.len);
-#endif
 		if ((gnl.ret_read = read(fd, buff, BUFFER_SIZE) == -1))
 			return(free_mem(&buff));
 		buff[gnl.ret_read] = '\0';
-#ifdef DEBUG
+																		#ifdef DEBUG
+printf(B_YEL "gnl.len = " RESET "[%lu]\n", gnl.len);
 printf(B_YEL "gnl.ret_read = " RESET "[%lu]\n", gnl.ret_read);
-printf(B_YEL "\nRead & last = \'\\0\'" B_GRN "[Ok]\n" RESET);
-printf(B_YEL "\nRead buff" RESET " = |%s|\n", buff);
-#endif
+printf(B_YEL "Read & last = \'\\0\'" B_GRN "[Ok]\n" RESET);
+printf(B_YEL "\nRead buff" B_WHT " = |%s|\n" RESET, buff);
+																		#endif
 		if (!(tmp = ft_strjoin(tmp, buff)))
 			return (free_mem(&buff));
 	}
-#ifdef DEBUG
+																		#ifdef DEBUG
 printf(B_YEL "\nWhile break " B_GRN "[Ok]\n" RESET);
-#endif
+																		#endif
 	*line = buff;
 	if (!gnl.ret_read)
 		return (EOF);
