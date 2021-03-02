26fa43e8693fefd8255c5edc959234ee538c4f9c
Matthew-Dreemurr <hadadmat@gmail.com>
2021-03-02 15:52:05 +0100

    push 2 lamour

--- include/wraloc.h
+++ include/wraloc.h
@@ -485,7 +485,7 @@ static void					_mem_print(t_mem *head)
 	printf(CR"\n");
 	}
 }
-/*
+#ifndef __APPLE__
 static int	vasprintf(char **strp, const char *fmt, va_list ap)
 {
 	va_list ap1;
@@ -501,7 +501,7 @@ static int	vasprintf(char **strp, const char *fmt, va_list ap)
 	*strp = buffer;
 	return vsnprintf(buffer, size, fmt, ap);
 }
-*/
+
 static int	asprintf(char **strp, const char *fmt, ...)
 {
 	int error;
@@ -512,7 +512,7 @@ static int	asprintf(char **strp, const char *fmt, ...)
 	va_end(ap);
 	return error;
 }
-
+#endif
 # ifdef malloc
 #  undef malloc
 # endif
