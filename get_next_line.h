230ce4c457be328d7eab3c38d390c3c9d205a116
Matthew_Dreemurr <hadadmat@gmail.com>
2021-03-09 12:28:31 +0100

    wip exe:[OK] norm:{KO}

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/02 14:18:42 by mhadad            #+#    #+#             */
-/*   Updated: 2021/03/09 11:34:10 by mhadad           ###   ########.fr       */
+/*   Updated: 2021/03/09 11:46:34 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include <unistd.h>
 # include <limits.h>
 # include <stdio.h>
+#include "wraloc.h"
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE	128
