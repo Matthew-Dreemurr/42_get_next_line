5c408e27958bf30e0d4a0272e478fbd876c2359d
Matthew-Dreemurr <hadadmat@gmail.com>
2021-03-09 17:18:44 +0100

    test wip exe: [OK] norm: {KO}

--- get_next_line_main.c
+++ get_next_line_main.c
@@ -3,56 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_main.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mhadad <mhadad@student.s19.be>             ++  +:+       ++        */
-/*                                                ++++++   ++           */
-/*   Created: 2021/01/02 15:41:33 by mhadad            +    +             */
-/*   Updated: 2021/01/15 15:57:08 by mhadad              .fr       */
+/*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/09 16:05:34 by jaleman           #+#    #+#             */
+/*   Updated: 2021/03/09 17:14:38 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
-#include <strings.h>
-#include <string.h>
-#include<time.h>
+#include <sys/uio.h>
+#include <sys/types.h>
 
-#ifndef TXT
-# define TXT "ERROR"
-# define TXT2 "ERROR"
-#endif
-#ifndef LOOP
-# define LOOP 1
-#endif
+int	main(int argc, char const *argv[]) {
+	int fd;
+	int fd_2;
+	int i;
+	char *line;
+	int status;
 
-int		main()
-{
-	time_t t;   // not a primitive datatype
-    time(&t);
-	char	*line;
-	int		ret;
-	int		fd;
-
-	printf("%s", ctime(&t));
-	fd = open(TXT, O_RDONLY);
-#ifdef DEBUG
-	puts("Open\n");
-#endif
-	while ((ret = get_next_line(fd, &line)) > 0)
+	fd = open(argv[1], O_RDONLY);
+	i = 1;
+	while ((status = get_next_line(fd, &line)) == 1 && i < 500)
 	{
-#ifdef DEBUG
-		printf("\nGnl ret: %d\n", ret);
-#endif
-		printf(YEL "Line: |%s|\n" RESET, line);
+		printf("[%d] %s\n", i++, line);
 		free(line);
 	}
-#ifdef DEBUG
-		printf("\nEOF Gnl ret:\n|%d|\n", ret);
-#endif
-		printf(YEL "\nLine: |%s|\n" RESET, line);
+	fd_2 = open(argv[2], O_RDONLY);
+	close(fd);
+	fd = fd_2;
+	while ((status = get_next_line(fd, &line)) == 1)
+	{
+		printf("[%d] %s\n", i++, line);
 		free(line);
-		close(fd);
-	return (0);
+	}
+	return (argc);
 }
