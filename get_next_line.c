18a64cee68e59eddd308f66e99a7574f35af16d7
Matthew_Dreemurr <hadadmat@gmail.com>
2021-03-09 09:46:34 +0100

    remake

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/02 14:15:17 by mhadad            #+#    #+#             */
-/*   Updated: 2021/03/08 15:53:03 by mhadad           ###   ########.fr       */
+/*   Updated: 2021/03/09 09:46:22 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,33 +22,5 @@
 
 int		get_next_line(int fd, char **line)
 {
-	static ssize_t	read_ret = 1;
-	static char		*tmp;
-	char			*buff;
 
-	if (fd < 0 || !line || BUFFER_SIZE <= 0)
-		return(ERROR);
-	if (read_ret == 0 && !lenStr(tmp, '\n', 2))
-	{
-		*line = joinStr(NULL, NULL);
-		return (freeRetun((void*)&tmp, EO_FILE));
-	}
-	if (!(buff = (char*)malloc(BUFFER_SIZE + 1)))
-		return (ERROR);
-	buff[BUFFER_SIZE] = '\0';
-	while (!(lenStr(tmp, '\n', 2)) && (read_ret != 0))
-	{
-		if ((read_ret = read(fd, buff, BUFFER_SIZE)) == ERROR)
-			return (freeRetun((void*)&buff, ERROR));
-		buff[read_ret] = '\0';
-		if (!(tmp = joinStr(tmp, buff)))
-		{
-			free(tmp);
-			return (freeRetun((void*)&buff, ERROR));
-		}
-	}
-	free(buff);
-	if (!(*line = nextLine(&tmp)))
-		return (freeRetun((void*)&tmp, ERROR));
-	return (L_READ);
 }
