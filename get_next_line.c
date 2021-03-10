260bb2daa33151a8bdbc8d728574656195ffdaed
Matthew-Dreemurr <hadadmat@gmail.com>
2021-03-10 13:46:52 +0100

    remake

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mhadad <mhadad@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/02 14:15:17 by mhadad            #+#    #+#             */
-/*   Updated: 2021/03/09 15:10:52 by mhadad           ###   ########.fr       */
+/*   Updated: 2021/03/10 13:46:45 by mhadad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,24 +32,4 @@ int		get_next_line(int fd, char **line)
 	if (!(box.buff = (char*)malloc(BUFFER_SIZE + 1)))
 		return (ERROR);
 	box.buff[BUFFER_SIZE] = '\0';
-	box.readRet = BUFFER_SIZE;
-	while (!(lenStr(box.buff, '\n', 2)) && !(box.readRet < BUFFER_SIZE))
-	{
-		//printf("=== while ===");
-		if ((box.readRet = read(fd, box.buff, BUFFER_SIZE)) == ERROR)
-			return (ERROR);
-		box.buff[box.readRet] = '\0';
-		if (!(box.tmp[fd] = joinStr(box.tmp[fd], box.buff, TRUE, FALSE)))
-			return (ERROR);
-	}
-	free(box.buff);
-	//printf("tmp @ end: |%s|\n", box.tmp[fd]);
-	if (box.readRet < BUFFER_SIZE && !(lenStr(box.tmp[fd], '\n', 2)))
-	{
-		*line = joinStr(box.tmp[fd], NULL, FALSE, FALSE);
-		return (freeRetun((void*)&(box).tmp[fd], EO_FILE));
-	}
-	if (!(*line = nextLine(&(box).tmp[fd])))
-		return (freeRetun((void*)&(box).tmp[fd], ERROR));
-	return (L_READ);
 }
